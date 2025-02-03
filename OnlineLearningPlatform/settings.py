afd29b94475f69d32e12de473af78ab980ca0682
Praveesh S <praveeshsuvarna02@gmail.com>
2025-02-03 09:15:32 +0530

    First Commit

--- OnlineLearningPlatform/settings.py
+++ OnlineLearningPlatform/settings.py
@@ -140,8 +140,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'otp2455@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'gunj hspp lqnq sxql'  # The app-specific password generated from Gmail
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
